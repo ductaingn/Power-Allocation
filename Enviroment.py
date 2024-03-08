adc8671831ab39b75ef403c27ad6a2042cafe012
R4C00n <superc0605@gmail.com>
2024-03-08 22:44:11 +0700

    New action set

--- Enviroment.py
+++ Enviroment.py
@@ -3,12 +3,8 @@ import random as rd
 import matplotlib.pyplot as plt
 
 # the considered space has a width of 90 meters, a length of 90 meters
-length = 90
-width = 90
-
-length_of_cell = 0.001
-number_of_area_per_row = 3
-length_of_area = length/number_of_area_per_row
+length = 150
+width = 150
 
 # Number of APs
 NUM_OF_AP = 1
@@ -18,9 +14,9 @@ NUM_OF_DEVICE = 3
 NUM_OF_SUB_CHANNEL = 4
 NUM_OF_BEAM = 4
 # Transmit Power P_sub = P_mW = P ~ 5dBm
-P = pow(10, 5/10)
+P = pow(10, 5/10)*1e-3
 # Noise Power sigma^2 ~ -169dBm/Hz
-SIGMA_SQR = pow(10, -169/10)
+SIGMA_SQR = pow(10, -169/10)*1e-3
 # Bandwidth Sub6-GHz = 100MHz, W_mW = 1GHz
 # Bandwidth per subchannel W_sub = 100MHz/number of sub channel
 W_SUB = 1e8/NUM_OF_SUB_CHANNEL
@@ -28,28 +24,31 @@ W_MW = 1e9
 # Number of levels of quantitized Transmit Power
 A = 10
 # Emitting power constraints P_min = 5 dBm, P_max = 38 dBm 
-P_MIN = pow(10,5/10)
-P_MAX = pow(10,38/10)
+P_MIN = pow(10,5/10)*1e-3
+P_MAX = pow(10,38/10)*1e-3
 # Power set
 POWER_SET = [0,P_MIN]
 for i in range(3,A+1):
     POWER_SET.append((P_MAX-P_MIN)*i/A)
-# Frame Duration T_s
-T = 1
-# Packet size D = 8 bit
-D = 8
+# Frame Duration T_s 
+T = 1e-3
+# Packet size D = 8000 bit
+D = 8000
+# Number of frame
+NUM_OF_FRAME = 10000
+# LoS Path loss - mmWave
+LOS_PATH_LOSS = np.random.normal(0,5.8,NUM_OF_FRAME)
+# NLoS Path loss - mmWave
+NLOS_PATH_LOSS = np.random.normal(0,8.7,NUM_OF_FRAME) 
 
 # initialize position of AP.
 # the AP was located at the central of the area
 # the position of each AP is the constant
-AP_POSITION = (45, 45)
+AP_POSITION = (0, 0)
 
 # the function calculates the distance to the nearest AP
-
-
 def distance_to_AP(pos_of_device):
-    distance = np.sqrt((pos_of_device[0] - AP_POSITION[0]) * (pos_of_device[0] - AP_POSITION[0]) + (
-        pos_of_device[1] - AP_POSITION[1]) * (pos_of_device[1] - AP_POSITION[1]))
+    distance = np.sqrt((pos_of_device[0] - AP_POSITION[0])**2 + (pos_of_device[1] - AP_POSITION[1])**2)
     return distance
 
 
@@ -62,25 +61,20 @@ def initialize_devices_pos():
     for i in range (NUM_OF_DEVICE):
         # Distance from Device #1 to AP and Device #2 to AP is equal
         if(i==0):
-            x = rd.uniform(30,60)
-            y = rd.uniform(30,60)
+            x = 0
+            y = 20
         elif(i==1):
-            distance_d0 = distance_to_AP(list_of_devices[0])
-            x = rd.uniform(AP_POSITION[0]-distance_d0,AP_POSITION[0]+distance_d0)
-            y = AP_POSITION[1]-np.sqrt(distance_to_AP(list_of_devices[0])**2-(x-AP_POSITION[0])**2)
-            y = y if y>0 else -y
+            x = 20
+            y = 0
         
         # Distance from Device #3 to AP is greater than from Device #1 and #2 
         elif(i==2):
-            x = rd.uniform(0,length)
-            y = rd.uniform(0,width)
-            while(distance_to_AP(list_of_devices[0]) > 1.5*distance_to_AP([x,y])):
-                x = rd.uniform(0,length)
-                y = rd.uniform(0,width)
+            x = -55
+            y = -60
 
         else:
-            x = rd.uniform(0,length)
-            y = rd.uniform(0,width)
+            x = rd.uniform(-length/2,length/2)
+            y = rd.uniform(-width/2,width/2)
 
         list_of_devices.append((x,y))
     return list_of_devices
@@ -91,19 +85,19 @@ def path_loss_sub(distance):
     return 38.5 + 30*(np.log10(distance))
 
 
-def path_loss_mW_los(distance):
-    X = np.random.normal(0, 5.8)
+def path_loss_mW_los(distance,frame):
+    X = LOS_PATH_LOSS[frame]
     return 61.4 + 20*(np.log10(distance))+X
 
 
-def path_loss_mw_nlos(distance):
-    X = np.random.normal(0, 8.7)
+def path_loss_mw_nlos(distance,frame):
+    X = NLOS_PATH_LOSS[frame]
     return 72 + 29.2*(np.log10(distance))+X
 
 
 # Main Transmit Beam Gain G_b
-def G(eta, beta):
-    epsilon = 0.1
+def G(eta=5*np.pi/180, beta=0):
+    epsilon = 0.01
     return (2*np.pi-(2*np.pi-eta)*epsilon)/(eta)
 
 
@@ -120,27 +114,22 @@ def generate_h_tilde(mu, sigma, amount):
 
 
 def compute_h_sub(list_of_devices, device_index, h_tilde):
-    h = h_tilde * \
-        pow(10, -
-            path_loss_sub(distance_to_AP(list_of_devices[device_index]))/20.0)
-    return np.power(np.abs(h), 2)
+    h = np.abs(h_tilde* pow(10, -path_loss_sub(distance_to_AP(list_of_devices[device_index]))/20.0))**2
+    return h
 
 
-def compute_h_mW(list_of_devices, device_index, eta, beta, h_tilde):
+def compute_h_mW(list_of_devices, device_index, h_tilde, frame,eta=5*np.pi/180, beta=0):
+    h = 0
     # device blocked by obstacle
     if (device_index == 1 or device_index == 5):
-        path_loss = path_loss_mw_nlos(
-            distance_to_AP(list_of_devices[device_index]))
-        h = G(eta, beta)*np.abs(h_tilde) * \
-            pow(10, -path_loss/20)*0.1  # G_Rx^k=epsilon
+        path_loss = path_loss_mw_nlos(distance_to_AP(list_of_devices[device_index]),frame)
+        h = np.abs(G(eta, beta)*h_tilde* pow(10, -path_loss/20)*0.01)**2  # G_Rx^k=epsilon
     # device not blocked
     else:
-        path_loss = path_loss_mW_los(
-            distance_to_AP(list_of_devices[device_index]))
-        h = pow(G(eta, beta)*np.abs(h_tilde) *
-                pow(10, -path_loss/20), 2)  # G_Rx^k = G_b
+        path_loss = path_loss_mW_los(distance_to_AP(list_of_devices[device_index]),frame)
+        h = np.abs((G(eta, beta)**2)*h_tilde * pow(10, -path_loss/20))**2  # G_Rx^k = G_b
 
-    return np.power(np.abs(h), 2)
+    return h
 
 
 # check if sum of all power on interface <= P_max ?
@@ -186,13 +175,10 @@ def l_max(r_bkv):
     return r_bkv*T/D
 
 # Number of sucessfully received packets at device k on interface v
-
-
 def num_of_success_packets(l_kv, l_kv_max):
     return l_kv-l_kv_max
+    
 # Packet Successful Delivery rate at device k on interface v
-
-
 def packet_successful_rate(num_of_success_packet, num_of_packet):
     return num_of_success_packet/num_of_packet
 
@@ -205,4 +191,3 @@ def packet_loss_rate(t, old_packet_loss_rate, packet_successful_rate, l_kv):
         packet_loss_rate = (
             1/t)*((t-1)*old_packet_loss_rate + (1-packet_successful_rate))
         return packet_loss_rate
-
