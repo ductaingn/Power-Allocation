f9a1920ac200eca5852e8fede0b27ac30071ec1b
R4C00n <superc0605@gmail.com>
2024-03-11 14:07:29 +0700

    Fix typo

--- generate_possible_action.py
+++ generate_possible_action.py
@@ -2,29 +2,28 @@ import Enviroment as env
 import IO
 import numpy as np
 
-actions = []
-device = []
-action = [0,0]
-sub_channel = [0,0,0,0]
-mW_beam = [0,0,0,0]
+actions = [] # Contains all combinations of action
+device = [] # Contains combinations of (interface, power level) of one device
+action_device = [0,0]
+sub_channel = np.full(env.NUM_OF_SUB_CHANNEL,0)
+mW_beam = np.full(env.NUM_OF_BEAM,0)
 
-def gen_sub(device,action,k):
+def gen_one_device(device,action,k):
     if(k==2):
         device.append(action.copy())
         return
     if(k==0):
         for i in range(3):
             action[k]=i
-            gen_sub(device,action,k+1)
+            gen_one_device(device,action,k+1)
     else:
         # number of beam = number of subchannel
         for i in range(env.NUM_OF_BEAM):
             action[k]=i
-            gen_sub(device,action,k+1)
+            gen_one_device(device,action,k+1)
 
-gen_sub(device,action,0)
+gen_one_device(device,action_device,0)
 allocated = np.full(len(device),0)
-action = [0,0,0]
 def mark_allocated(i):
     interface = device[i][0]
     if(interface == 0):
@@ -49,6 +48,7 @@ def mark_unallocated(i):
                 allocated[j] = 0
         return
     
+action = [0,0,0]
 def gen(actions,action,k):
     if(k==env.NUM_OF_DEVICE):
         # actions.append(tuple([tuple(row) for row in  action.copy()]))
